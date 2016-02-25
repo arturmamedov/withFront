72c8022f7b4daa596a80fc025da31c83b313096a
Artur Mamedov <artur@buonsito.it>
2016-02-25 14:47:13 +0100

    Updated withplusgins.js with new javascript tools and tricks, ready for use

--- withplugins.js
+++ withplugins.js
@@ -43,6 +43,120 @@ $("body").on('click', ".withAlert .close", function(){
 /* jQuery plugins and other thing that need to be run after the document is load */	
 $(document).ready(function () {
 	/**
+	 * Form that need be send with Ajax and with CakePHP 3.x  
+	$(".ajaxform").on('submit', function(e){
+        e.preventDefault();
+        var area_code = $(this).data('areacode'), thisForm = $(this);
+
+        $('input, select, textarea').parent().removeClass('has-error').find('.help-block').remove();
+
+        $.ajax({
+            url: $(this).attr('action'),
+            method: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            success: function(json){
+                if(json.success)
+				{
+                    $("body").gdivMessage(json.message, 'success');
+
+					if($(thisForm).data('scallback').length > 0)
+					{
+						eval($(thisForm).data('scallback'));
+					}
+                } else {
+                    $("body").gdivMessage(json.message, 'danger');
+
+                    for(err in json.errors){
+                        $('.form-'+err, $('#tab_'+area_code))
+                            .after('<span class="help-block alert alert-danger">'+ json.errors[err] +'</span>')
+                            .parent().addClass('has-error');
+                    }
+
+					if($(thisForm).data('ecallback').length > 0)
+					{
+						eval($(thisForm).data('ecallback'));
+					}
+                }
+            },
+            error: function(){
+                $("body").gdivMessage();
+
+				if($(thisForm).data('fcallback').length > 0)
+				{
+					eval($(thisForm).data('fcallback'));
+				}
+            }
+        });
+
+		//if($(thisForm).data('callback').length > 0)
+		//{
+		//	eval($(thisForm).data('callback'));
+		//}
+
+        return false;
+    });*/
+	
+	/**
+	 * init nicescroll 
+	 */
+	$("html, .withNicescroll").niceScroll();
+	
+	/**
+	 * Add target highlight to something
+	 */
+	$("body").on('click', '.targetLink', function(){
+        var elem = $(this).data('target');
+        $(elem).addClass('target');
+		if($(this).data('auto-close') != 'false'){
+			var ac = parseInt($(this).data('auto-close'));
+			if(ac == 0){
+				ac = 3500;
+			}
+			setTimeout(function(){$(elem).removeClass('target');}, ac);
+		}
+    });
+
+    /**
+	 * START: Layout Go2Top
+	 */
+    var LayoutGo2Top = function () {
+        var handle = function () {
+            var currentWindowPosition = $(window).scrollTop(); // current vertical position
+            if (currentWindowPosition > 300) {
+                $(".go2top").show();
+            } else {
+                $(".go2top").hide();
+            }
+        };
+
+        return {
+            //main function to initiate the module
+            init: function () {
+                handle(); // call headerFix() when the page was loaded
+                if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+                    $(window).bind("touchend touchcancel touchleave", function (e) {
+                        handle();
+                    });
+                } else {
+                    $(window).scroll(function () {
+                        handle();
+                    });
+                }
+
+                $(".go2top").on('click', function (e) {
+                    e.preventDefault();
+                    $("html, body").animate({
+                        scrollTop: 0
+                    }, 600);
+                });
+            }
+        };
+    }();
+    LayoutGo2Top.init();
+    /** END: Layout Go2Top */
+	
+	/**
 	 * Raty - https://github.com/wbotelhos/raty
      * with data API initialization from https://github.com/wbotelhos/raty/pull/168
      * get only the star type or set my prefered icon type and all others from data-api:
@@ -53,5 +167,41 @@ $(document).ready(function () {
         if(typeof startype == 'undefined' || startype.length == 0)
             startype = 'i';
         $(this).raty({starType: startype});
+    });
+	
+	/**
+	 * withBox
+	 * #showhide https://gist.github.com/arturmamedov/2bdfc3f69828ac37a7a1
+	 */
+    $(".withBox").on('click', ".showhideBox", function(){
+        var thisBox = $(this), parent = false;
+        while(parent == false){
+            thisBox = thisBox.parent();
+            if(thisBox.hasClass('withBox')){
+                parent = true;
+            }
+        }
+        
+        $(".shBox", thisBox).toggle(200);
+        $(".shSwitch", thisBox).toggle(1, function(){
+            if($(this).hasClass('set'))
+                $(this).removeClass('set');
+            else
+                $(this).addClass('set');
+        });
+    });
+    // no close on himself click
+    $(".withBox.collapsable").mouseup(function(){ return false; });
+    // close on document click
+    $(document).mouseup(function(){
+        $(".shBox", $(".withBox.collapsable")).slideUp();
+        var shSwitch = $(".shSwitch",  $(".withBox.collapsable"));
+        if(shSwitch.hasClass('set'))
+            shSwitch.toggle(1, function(){
+                if($(this).hasClass('set'))
+                    $(this).removeClass('set');
+                else
+                    $(this).addClass('set');
+            });
     });
 });
