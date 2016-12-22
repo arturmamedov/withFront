f7a1769905e11c6735d6639447d1baabd3266756
Artur Mamedov <arturmamedov1993@gmail.com>
2016-12-22 21:25:29 +0100

    withPlugins - Raty default values for startype, readonly, score

--- withplugins.js
+++ withplugins.js
@@ -469,16 +469,30 @@ $(document).ready(function () {
     if ($().raty) {
         /**
          * Raty - https://github.com/wbotelhos/raty
-         * with data API initialization from https://github.com/wbotelhos/raty/pull/168
+         * settable trough data-api: startype = i, readonly = true, score = 5
+         *
+         * or if use with data API initialization from https://github.com/wbotelhos/raty/pull/168
          * get only the star type or set my prefered icon type and all others from data-api:
          * <span class="getraty" data-score="5" data-star-on="fa fa-star" data-star-off="" data-read-only="true"></span>
          *
          */
         $('.getraty').each(function () {
+            // startype = i
             var startype = $(this).data('startype');
             if (typeof startype == 'undefined' || startype.length == 0)
                 startype = 'i';
-            $(this).raty({starType: startype});
+
+            // readonly = true
+            var readonly = $(this).data('readonly');
+            if (typeof readonly == 'undefined' || readonly.length == 0)
+                readonly = true;
+
+            // score = 5
+            var score = $(this).data('score');
+            if (typeof score == 'undefined' || score.length == 0)
+                score = 5;
+
+            $(this).raty({starType: startype, readOnly: readonly, score: score});
         });
     } // END - #jQuery.raty
 
