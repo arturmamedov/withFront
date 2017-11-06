67957af3d2d010365e3eb274bee92933824b4a8f
Artur Mamedov <arturmamedov1993@gmail.com>
2017-11-06 11:36:06 +0100

    withPlugins - Hash Navigation select by class and not id, in case of multiple tabs

--- withplugins.js
+++ withplugins.js
@@ -382,11 +382,11 @@ $(document).ready(function () {
      # # # # # # # hash-navigation # for bootstrap tabs # # # # # # # # #
      ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **/
     var hash = window.location.hash;
-    if (hash.length > 0 && $('#hash-navigation').length > 0) {
+    if (hash.length > 0 && $('.hash-navigation').length > 0) {
         $('a[href=' + hash + ']').tab('show');
     }
     // # look for changes in navigation and add it to url
-    $('#hash-navigation a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.hash-navigation a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         return location.hash = $(e.target).attr('href').substr(1);
     });
     /** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
