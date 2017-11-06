591671fb89cacff06468ed061eb3e293710740d8
Artur Mamedov <arturmamedov1993@gmail.com>
2017-11-06 11:42:53 +0100

    withPlugins/Style - Added modal-wide trick, by adding a .modal-wide class to bootstrap modal we have a full page modal

--- withplugins.js
+++ withplugins.js
@@ -620,6 +620,13 @@ $(document).ready(function () {
         }
     });
 
+    // bootstrap wide modal - adjust height to fit entire page
+    $(".modalWide").on("show.bs.modal", function() {
+        var height = $(window).height() - 200;
+        $(this).find(".modal-body").css("max-height", height);
+    });
+    // END - bootstrap wide modal
+
     /**
      * START: Layout Go2Top
      */
