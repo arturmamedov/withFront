fc2b3fcbeaeb9c5b20748ac229ec02cf23b68c86
Artur Mamedov <arturmamedov1993@gmail.com>
2017-11-06 11:39:01 +0100

    withPlugins - Added window on buffer onload functionality, commented because it not needed in every site

--- withplugins.js
+++ withplugins.js
@@ -1,3 +1,53 @@
+/* * * * * * * * * * * * *
+ *        Start          *
+ * Window on buffer onload
+ * * * * * * * * * * * * */
+// Message to be displayed on unload
+// var message = $('#jtrnslt #leavemess').text(), submitting = false;
+// // On Form submition set submitting to true for not fire onbeforeunload
+// $(document).on("submit", "form", function(event){
+//     submitting = true;
+// });
+// // Window onbeforeunload fired when leave page but not if it a form submition or element with .noBeforeUnload class
+// window.onbeforeunload = function (e) {
+//     e = e || window.event;
+//     // Determine if you want to allow unload
+//     /* some synchronous operation here*/
+//     if (!submitting && !$(e.target.activeElement).hasClass('noBeforeUnload')) {
+//         // IE BUG: throws unspecified error on "cancel"
+//         // so we temporarily mask all Javascript errors
+//         window.onerror = function (msg, url, linenumber) {
+//             return -1 < msg.toLowerCase().indexOf('unspecified error');
+//         };
+//         // After return the (blocking) confirmation window is shown
+//         // after which we remove Javascript error handler
+//         window.setTimeout(function () {
+//             window.onerror = function () {
+//                 return false;
+//             }
+//         }, 100);
+//         // -- IE BUG
+//
+//         // default message if missed
+//         if (message.length == 0) {
+//             message = 'Are you sure you want leave this page without save? Sei sicuro di volere uscire senza salvare?';
+//         }
+//
+//         // IE expects the confirmation string in the
+//         // `returnValue` property of event object
+//         if (e) {
+//             e.returnValue = message;
+//         }
+//         // Other browsers expect it returned as a string
+//         return message;
+//     }
+// };
+/* * * * * * * * * * * * *
+ *          END          *
+ * Window on buffer onload
+ * * * * * * * * * * * * */
+
+
 /* * * * * * * * * * * * * * * * * *
  * * * * *  gDiv Message * * * * * *
  need Bootstrap alert and some css:
