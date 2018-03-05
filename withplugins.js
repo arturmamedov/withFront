6f1d21eaefd4fdcc0825c91e6ef30837b48f4151
Artur Mamedov <arturmamedov1993@gmail.com>
2018-03-05 16:27:49 +0100

    js/Google Anlytics - Old Analytics code `_gaq` methods commented for switch if necessary

--- withplugins.js
+++ withplugins.js
@@ -339,43 +339,67 @@ $(document).ready(function () {
 
     /**
      * Google Analytics link click/event tracking
+     * also with old _gaq commented if u use old google analytics code https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApi_gaq#push
      * /
      $("a").click(function () {
         if (!$(this).hasClass("gadisabled")) {
-            var href = $(this).attr('href');
+             var href = $(this).attr('href');
 
             if (typeof ga !== "undefined" && typeof href !== "undefined") {
                 if (href.indexOf('mailto:') >= 0) {
                     ga('send', 'event', 'contatti', 'click', href.replace('mailto:', ''), '5');
+                    // _gaq.push(['_trackEvent', 'contatti', 'click', href.replace('mailto:', ''), 5]);
                 }
 
                 if (href.indexOf('tel:') >= 0) {
                     ga('send', 'event', 'contatti', 'click', href.replace('tel:', ''), '5');
+                    // _gaq.push(['_trackEvent', 'contatti', 'click', href.replace('tel:', ''), 5]);
+                }
+
+                if (href.indexOf('skype:') >= 0) {
+                    ga('send', 'event', 'contatti', 'click', href.replace('skype:', ''), '5');
+                    // _gaq.push(['_trackEvent', 'contatti', 'click', href.replace('skype:', ''), 5]);
                 }
 
                 if (href.indexOf('maps') >= 0) {
                     ga('send', 'event', 'contatti', 'click', 'maps', '5');
+                    // _gaq.push(['_trackEvent', 'contatti', 'click', 'maps', 5]);
+                }
+
+                if (href.indexOf('instagram.com') >= 0) {
+                    ga('send', 'event', 'contatti', 'click', 'instagram', '5');
+                    // _gaq.push(['_trackEvent', 'social', 'click', 'instagram', 2]);
                 }
 
                 if (href.indexOf('twitter.com') >= 0) {
                     ga('send', 'event', 'social', 'click', 'twitter', '2');
+                    // _gaq.push(['_trackEvent', 'social', 'click', 'twitter', 2]);
                 }
 
                 if (href.indexOf('facebook.com') >= 0) {
                     ga('send', 'event', 'social', 'click', 'facebook', '2');
+                    // _gaq.push(['_trackEvent', 'social', 'click', 'facebook', 2]);
                 }
 
                 if (href.indexOf('google.com') >= 0) {
                     ga('send', 'event', 'social', 'click', 'google', '2');
+                    // _gaq.push(['_trackEvent', 'social', 'click', 'google', 2]);
+                }
+
+                if (href.indexOf('youtube.com') >= 0) {
+                    ga('send', 'event', 'social', 'click', 'youtube', '2');
+                    // _gaq.push(['_trackEvent', 'social', 'click', 'youtube', 2]);
                 }
 
                 if ((href.indexOf('sitename.com') < 0) && (href.indexOf('http') >= 0)) {
                     ga('send', 'event', 'outgoing', 'click', href);
+                    // _gaq.push(['_trackEvent', 'outgoing', 'click', href]);
                 }
 
                 if (href.match('.pdf$') != null) {
                     //var pdf_file_name = href.substr(href.lastIndexOf('/') + 1);
-                    ga('send', 'event', 'download', 'click', 'scheda-tecnica', '2');
+                    ga('send', 'event', 'download', 'click', 'label-evento', '2');
+                    // _gaq.push(['_trackEvent', 'download', 'click', 'label-evento', '2']);
                 }
             }
         }
@@ -386,8 +410,9 @@ $(document).ready(function () {
      * Google Analytics submit
      * /
      $(".gasubmit").submit(function () {
-        ga('send', 'pageview', '/email-form-contatti'); // for booking "/calcola-preventivo"
-    });
+        ga('send', 'pageview', '/email-form-contatti'); // for booking: (`.gabookingsubmit` "/calcola-preventivo");
+        // _gaq.push(['_trackPageview', '/email-form-contatti']);
+     });
      /* Google Analytics - can be disabled and commented */
 
     /* * * * * Ajax Form - ContactsController()->ajaxsend() * * * */
@@ -415,8 +440,8 @@ $(document).ready(function () {
                 if (json.success) {
                     // Google Analytics track (can be disabled and commented)
                     // if (typeof ga !== "undefined") {
-                    //     _gaq.push(['_trackPageview', '/form-contatti']);
                     //     ga('send', 'pageview', '/email-form-contatti');
+                    //     _gaq.push(['_trackPageview', '/email-form-contatti']);
                     //     ga('send', 'event', 'contatti', 'click', 'newsletter', '5');
                     // }
 
