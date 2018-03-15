648cec90a2de70ff984305194694cc52abc5050f
Artur Mamedov <arturmamedov1993@gmail.com>
2018-03-15 11:01:37 +0100

    JS/withPlugins - Updated nicescroll style and functionality

--- withplugins.js
+++ withplugins.js
@@ -598,20 +598,39 @@ $(document).ready(function () {
 
     // #jQuery.plugin - Nicescroll
     if ($().niceScroll) {
-        var nice = $("html").niceScroll({
-            cursorcolor: "#ccc",
-            cursorborder: "0px solid #fff",
+        $("html").niceScroll({
+            touchbehavior:false,
+            background:"#e2e2e2",
+            cursoropacitymin:1,
+            cursorcolor:"#141414",
+            cursoropacitymax:0.6,
+            cursorwidth:5,
+            cursorborder:'0px solid #fff',
+            railalign:"right",
             railpadding: {top: 0, right: 0, left: 0, bottom: 0},
-            cursorwidth: "5px",
             cursorborderradius: "0px",
-            cursoropacitymin: 0,
-            cursoropacitymax: 0.7,
             boxzoom: true,
             horizrailenabled: false,
             autohidemode: false
         });
 
-        $(".withNicescroll").niceScroll();
+        $(".withNicescroll").niceScroll({
+            touchbehavior:false,
+            background:"#e2e2e2",
+            cursoropacitymin:1,
+            cursorcolor:"#141414",
+            cursoropacitymax:0.6,
+            cursorwidth:5,
+            cursorborder:'0px solid #fff',
+            railalign:"right",
+            railpadding: {top: 0, right: 0, left: 0, bottom: 0},
+            cursorborderradius: "0px",
+            boxzoom: true,
+            horizrailenabled: false,
+            autohidemode: false
+        });
+
+        // fix horizontal
         $('html').addClass('no-overflow-y');
     } // END - jQuery.nicescroll
 
