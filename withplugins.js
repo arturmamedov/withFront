2211b0712b0203b73d244a23a43588710f0525ca
Artur Mamedov <arturmamedov1993@gmail.com>
2018-03-15 11:09:21 +0100

    JS/withPlugins - `.w-filter` Fixed bug for select inputs on Chrome

--- withplugins.js
+++ withplugins.js
@@ -214,11 +214,24 @@ $(document).ready(function () {
     });
 
 
-    // filter items on button click
+    /** w-filter
+     *  Filter items on button click or select change with data-filter=".selector"
+     */
     $(".w-filters").each(function(){
         var $grid = $(this);
-        $grid.on('click', '.w-filter', function() {
-            var filterValue = $(this).attr('data-filter');
+        $grid.on('click change', '.w-filter', function() {
+            var filterValue = '';
+
+            if($(this).is('select')) {
+                filterValue = $(this).val();
+            } else if ($(this).is('radio')) {
+                // @todo
+                // console.info('radio');
+                // console.info($(this).val());
+            } else {
+                filterValue = $(this).attr('data-filter');
+            }
+
             // hide all elements
             $(".w-item", $grid).hide();
             // show filtered
