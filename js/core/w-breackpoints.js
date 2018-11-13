71c503d4b8bc532c78992e7381c4a18f30350a9f
Artur Mamedov <arturmamedov1993@gmail.com>
2018-11-13 18:29:56 +0100

    wJS - w-breakpoints.js named corectly and enabled all breakpoints

--- js/core/w-breackpoints.js
+++
@@ -1,6 +0,0 @@
-// detect if the width of screen is bootstrap xs, sm, md, lg
-var isMobile = window.matchMedia("only screen and (max-width: 768px)");
-var isXs = window.matchMedia("(max-width: 768px)");
-// var isSm = window.matchMedia("(min-width: 768px) and (max-width: 991px)");
-// var isMd = window.matchMedia("(min-width: 992px) and (max-width: 1199px)");
-// var isLg = window.matchMedia("(min-width: 1200px)");
