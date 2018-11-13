9e859e3e034e5b75f9c91def63e814e75a19324f
Artur Mamedov <arturmamedov1993@gmail.com>
2018-11-13 18:29:26 +0100

    wJS - Added clog() function for debug when options.debug enabled

--- js/core/w-cookie.js
+++ js/core/w-cookie.js
@@ -6,9 +6,11 @@
  * .getJSON([name]);
  */
 if(typeof Cookies != 'undefined') {
+    clog('cookie are baked');
     wCookies = function() {
         return {
             get: function(name) {
+                clog('cookie-get(name):' + name);
                 return Cookies.get(name);
             },
             set: function(name, value, attributes) {
@@ -18,9 +20,11 @@ if(typeof Cookies != 'undefined') {
                 //     //path: '/', domain: '', // yet default
                 // };
                 // var attributes = $.extend(defaults, options);
+                clog('cookie-set(name, value, attributes):' + name + ', ' + value + ', ' + attributes);
                 Cookies.set(name, value, attributes);
             },
             remove: function(name, attributes) {
+                clog('cookie-remove(name, attributes):' + name + ', ' + attributes);
                 Cookies.remove(name, attributes);
             },
             getJSON: function (name) {
