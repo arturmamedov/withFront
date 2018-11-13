9d14730f33c83cf25b6131e197c18108e95fd795
Artur Mamedov <arturmamedov1993@gmail.com>
2018-11-13 18:46:39 +0100

    Core/Build - Created grunt file for build withplugins.js from all separated script files

---
+++ js/withplugins_concat_template.js
@@ -0,0 +1,56 @@
+/* withInclude('js/form/w-buffer_unload.js') */
+
+/* withInclude('js/core/w-core.js') */
+
+/* withInclude('js/core/w-cookie.js') */
+
+/* withInclude('js/core/w-alert.js') */
+
+/* withInclude('js/widget/w-loader.js') */
+
+/* jQuery plugins and other thing that need to be run after the document is load */
+$(document).ready(function () {
+    /* withInclude('js/form/w-honey_pot.js') */
+
+    /* withInclude('js/core/w-breakpoints.js') */
+
+    /* withInclude('js/style/w-height.js') */
+
+    /* withInclude('js/form/w-filter.js') */
+
+    /* withInclude('js/form/w-children_age.js') */
+
+    /* withInclude('js/form/w-ajaxsave.js') */
+
+    /* withInclude('js/web/w-analytics.js') */
+
+    /* withInclude('js/form/w-ajaxsend.js') */
+
+    /* withInclude('js/bs/w-tab_hashnav.js') */
+
+    /* withInclude('js/plugins/w-datepicker.js') */
+
+    /* withInclude('js/plugins/w-tooltip.js') */
+
+    /* withInclude('js/plugins/w-selectize.js') */
+
+    /* withInclude('js/plugins/w-bootstrap_switch.js') */
+
+    /* withInclude('js/plugins/w-nicescroll.js') */
+
+    /* withInclude('js/style/w-target.js') */
+
+    /* withInclude('js/bs/w-wide_modal.js') */
+
+    /* withInclude('js/widget/w-go2top.js') */
+
+    /* withInclude('js/widget/w-appear_btn.js') */
+
+    /* withInclude('js/plugins/w-raty.js') */
+
+    /* withInclude('js/style/w-showhide.js') */
+
+    /* withInclude('js/form/w-cookie.js') */
+
+    /* withInclude('js/form/w-binder.js') */
+});
