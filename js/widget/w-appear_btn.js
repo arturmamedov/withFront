07526b17d059c0709c7e11b749a2ec17b8d7a82f
Artur Mamedov <arturmamedov1993@gmail.com>
2019-02-19 17:39:31 +0100

    w-appear_btn.js - Added data conf with default for position from bottom data-bottom="60" and delay for show data-delay="1440"

--- js/widget/w-appear_btn.js
+++ js/widget/w-appear_btn.js
@@ -1,17 +1,23 @@
 /**
  * Animate the bottom appear button .wabb
  *
- *  <a type="button" href="javascript:;" class="wabb btn btn-primary">
+ *  <a type="button" href="javascript:;" class="wabb btn btn-primary" data-bottom="60" data-delay="1440">
  *      Bottom Button <i class="fa fa-check"></i>
  *  </a>
+ *
+ *  data-bottom: the position from bottom(60 default)
+ *  data-delay: after how many ms the button appear(1440 default)
  *
  **/
 var wAppearBottomButton = function () {
     return {
         init: function () {
+            var bottom_pos = $('.wabb').data('bottom') || 60,
+                delay = $('.wabb').data('delay') || 1440;
+
             setTimeout(function () {
-                $('.wabb').stop().animate({bottom: '33px'}, 800);
-            }, 1400);
+                $('.wabb').stop().animate({bottom: bottom_pos+'px'}, 800);
+            }, delay);
 
             if (withOptions.debug) {
                 console.info('wAppearBottomButton() enabled');
