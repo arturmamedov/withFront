d813e64b3091fce73ed918a00843a998780d87ec
Artur Mamedov <arturmamedov1993@gmail.com>
2019-03-22 10:06:54 +0100

    w-core/breakpoints - Added Bootstrap 4 breakpoints is4sm, is4md, is4lg, is4xl

--- js/core/w-breakpoints.js
+++ js/core/w-breakpoints.js
@@ -1,6 +1,10 @@
 // detect if the width of screen is bootstrap xs, sm, md, lg
-var isMobile = window.matchMedia("only screen and (max-width: 768px)");
-var isXs = window.matchMedia("(max-width: 768px)");
-var isSm = window.matchMedia("(min-width: 768px) and (max-width: 991px)");
-var isMd = window.matchMedia("(min-width: 992px) and (max-width: 1199px)");
-var isLg = window.matchMedia("(min-width: 1200px)");
+var isXs = window.matchMedia("(max-width: 768px)"),
+    isSm = window.matchMedia("(min-width: 768px) and (max-width: 991px)"),
+    isMd = window.matchMedia("(min-width: 992px) and (max-width: 1199px)"),
+    isLg = window.matchMedia("(min-width: 1200px)"),
+    //Bootstrap4 style
+    is4sm = window.matchMedia("(min-width: 576px)"),
+    is4md = window.matchMedia("(min-width: 768px)"),
+    is4lg = window.matchMedia("(min-width: 992px)"),
+    is4xl = window.matchMedia("(min-width: 1200px)");
