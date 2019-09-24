3d18acca99af64046035bc66f6c4f1a210940362
Artur Mamedov <arturmamedov1993@gmail.com>
2019-09-24 17:02:48 +0200

    Js/w-target - Improved functionality and added data-attributes

--- js/style/w-target.js
+++ js/style/w-target.js
@@ -1,16 +1,25 @@
 /**
  * Add target highlight to something
+ *
+ * data-target          [-]                CSS Selector for select the element on which apply
+ * data-auto-close      [8000]             Bool or the ms for close
+ * data-taregt-class    [on-target]        The class to add (default have CSS animation but it must be on the #anchor element also)
  */
-$("body").on('click', '.targetLink', function () {
+$("body").on('click', '.targetLink, .w-target', function () {
     var elem = $(this).data('target');
-    $(elem).addClass('target');
-    if ($(this).data('auto-close') != 'false') {
-        var ac = parseInt($(this).data('auto-close'));
-        if (ac == 0) {
-            ac = 3500;
+
+    var autoClose = (typeof $(this).data('autoClose') != 'undefined') ? $(this).data('autoClose') : 8000;
+    var targetClass = (typeof $(this).data('targetClass') != 'undefined') ? $(this).data('targetClass') : 'on-target';
+
+    $(elem).addClass(targetClass);
+
+    if (autoClose) {
+        if (autoClose === true) {
+            autoClose = 8000;
         }
+
         setTimeout(function () {
-            $(elem).removeClass('target');
-        }, ac);
+            $(elem).removeClass(targetClass);
+        }, autoClose);
     }
 });
