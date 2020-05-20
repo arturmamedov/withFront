dc3bd175623e62e4d82ffaf42aefea7d397740c3
Artur Mamedov <arturmamedov1993@gmail.com>
2020-05-20 19:00:08 +0200

    Build v1.5.8 - Added mailto2scroll and anlytics event, withAppeartopButton() .watb and update of all packages

--- dist/js/w-plugins.js
+++ dist/js/w-plugins.js
@@ -58,7 +58,8 @@
  */
 var withOptions = {
     debug: false, // enable/disable Debug mode
-    wAppearBottomButton: false, // enable/disable widget/w-appear_btn.js
+    wAppearBottomButton: false, // enable/disable widget/w-appear_btn.js -Bottom
+    wAppearTopButton: false, // enable/disable widget/w-appear_btn.js -Top
     go2top: true, // enable/disable widget/w-go2top.js
     htmlNicescroll: false,
     whatsappWeb: true, // replace WhatsApp mobile with Desktop on Desktop
@@ -1015,6 +1016,54 @@ var wAppearBottomButton = function () {
 
 if (withOptions.wAppearBottomButton) {
     wAppearBottomButton.init(); // with Appear Bottom Button {css: .wabb}
+}
+
+
+/**
+ * Animate the top appear button .watb .left .right
+ *
+ *  <a type="button" href="javascript:;" class="watb right btn btn-primary" data-top="60" data-delay="1440">
+ *      Top Button <i class="fa fa-check"></i>
+ *  </a>
+ *
+ *  data-top: the position from top(20 default)
+ *  data-right: with .right the position from bottom(CSS 105px default, 75px on mobile)
+ *  data-left: with .left the position from bottom(CSS 105px default, 75px on mobile)
+ *  data-delay: after how many ms the button appear(1440 default)
+ *
+ **/
+var wAppearTopButton = function () {
+    return {
+        init: function () {
+            var watb = $('.watb'),
+                top_pos = parseInt(watb.data('top')) || 20,
+                right_pos = parseInt(watb.data('right')),
+                left_pos = parseInt(watb.data('left')),
+                delay = watb.data('delay') || 1440;
+
+            if(!watb.hasClass('right') && !watb.hasClass('left')){
+                watb.addClass('right');
+            }
+            if (right_pos > 0) {
+                watb.css('right', right_pos+'px');
+            }
+            if (left_pos > 0) {
+                watb.css('left', left_pos+'px');
+            }
+
+            setTimeout(function () {
+                watb.stop().animate({top: top_pos+'px'}, 800);
+            }, delay);
+
+            if (withOptions.debug) {
+                console.info('wAppearTopButton() enabled');
+            }
+        }
+    };
+}();
+
+if (withOptions.wAppearTopButton) {
+    wAppearTopButton.init(); // with Appear Bottom Button {css: .watb}
 }
 
 
