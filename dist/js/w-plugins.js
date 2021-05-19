6cbadd774dbff5334dfde0ed365767976e8f005a
Artur Mamedov <arturmamedov1993@gmail.com>
2021-05-19 10:31:39 +0200

    Build with slick carousel included

--- dist/js/w-plugins.js
+++ dist/js/w-plugins.js
@@ -1463,6 +1463,49 @@ $('.w-binded.w-setter').each( function(){
 });
 });
 
+$(function(){
+    /* Slick activation other settings are made by data-slick="{}" api */
+    $(".slick-carousel").slick({
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        autoplay: true,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    dots: true,
+                    arrows: true,
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: false,
+                    arrows: true,
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: false,
+                    arrows: true,
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
 /*
  Copyright 2014 Google Inc. All rights reserved.
 
