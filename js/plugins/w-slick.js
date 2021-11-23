aec3235cd66cf55043fc04fef55703f48429f3af
Artur Mamedov <arturmamedov1993@gmail.com>
2021-11-23 16:00:18 +0100

    JS/plugins/slick - BUGFIX Added if statement for see if slick it loaded in page before use it, for not get error

--- js/plugins/w-slick.js
+++ js/plugins/w-slick.js
@@ -1,42 +1,44 @@
 $(function(){
-    /* Slick activation other settings are made by data-slick="{}" api */
-    $(".slick-carousel").slick({
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        autoplay: true,
-        dots: true,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    dots: true,
-                    arrows: true,
-                    infinite: true,
-                    slidesToShow: 3,
-                    slidesToScroll: 2
+    if ($().slick) {
+        /* Slick activation other settings are made by data-slick="{}" api */
+        $(".slick-carousel").slick({
+            slidesToShow: 4,
+            slidesToScroll: 3,
+            autoplay: true,
+            dots: true,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        dots: true,
+                        arrows: true,
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        dots: false,
+                        arrows: true,
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        dots: false,
+                        arrows: true,
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    dots: false,
-                    arrows: true,
-                    infinite: true,
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    dots: false,
-                    arrows: true,
-                    infinite: true,
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+            ]
+        });
+    }
 });
